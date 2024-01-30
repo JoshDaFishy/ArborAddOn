6e7cbea8ad2e12c6a631a02491b5f9e7884c2421
JoshDaFishy <58554128+JoshDaFishy@users.noreply.github.com>
2024-01-30 10:38:45 +0000

    Update Test.js
    Teachers figured out how to add a mark as submitted button themselves. But some forget to add it sometimes. You can use the post request from tasks with the button and it works fine for ones without the button if you change the ID. I may develop this so that it adds a mark as done button for tasks that wouldn't otherwise have one.

--- Test.js
+++ Test.js
@@ -14,32 +14,37 @@
 (function() {
     'use strict';
     console.log ("TEST");
-    var zNode = document.createElement ('div');
-    zNode.innerHTML = '<button id="MkDn" type="button">'
-        + 'Don\'t click me!</button>'
-    ;
-    zNode.setAttribute ('id', 'myContainer');
+    // Since teachers can now add a button to mark as done, I may repurpose this project so that on tasks where teachers forget to add the button it will add one manually, and use the post request that marks stuff as done.
     function actionFunction (jNode) {
         console.log ("TEST 2");
         console.log ("TEST 3");
+        console.log($('.mis-section:contains("Assignments that are due")').children().children().children().last().find(".x-field .mis-property-row"))
         //I need to now make it loop through each of the tasks and add the button to each. starts on 1013, and then counts to 1017
-        var task = document.getElementById ("mis-property-row-1013-bodyEl");
-        //var Node1 = task[0];
-        task.appendChild(zNode);
-        console.log (zNode);
-        document.getElementById ("MkDn").addEventListener (
-            "click", ButtonClickAction, false
-        );
+        // Find each jQuery
+        for (let i = 1013; i < 1100; i++) {
+            break;
+            var zNode = document.createElement ('div');
+            zNode.innerHTML = `<button id="MkDn${i}"  type="button">`
+                + 'Don\'t click me!</button>'
+            ;
+            zNode.setAttribute ('id', `myContainer${i}`);
+            var task = document.getElementById (`mis-property-row-${i}-bodyEl`);
+            //var Node1 = task[0];
+            task.appendChild(zNode);
+            console.log (zNode);
+            document.getElementById (`MkDn${i}`).addEventListener (
+                "click", function ButtonClickAction (zEvent) {
+                    var zNode = document.createElement ('p');
+                    zNode.innerHTML = 'The button was clicked.';
+                    document.getElementById (`myContainer${i}`).appendChild (zNode);
+                }, false
+            );
+        }
     }
     //--- Activate the newly added button.
     window.addEventListener("load", function(e){
         waitForKeyElements (".calendar-widget-header", actionFunction);
     })
-    function ButtonClickAction (zEvent) {
-        var zNode = document.createElement ('p');
-        zNode.innerHTML = 'The button was clicked.';
-        document.getElementById ("myContainer").appendChild (zNode);
-    };
 
     //--- Style our newly added elements using CSS.
     GM_addStyle ( `
@@ -63,5 +68,4 @@
         background:             white;
     }
 ` );
-    // Your code here...
 })();
